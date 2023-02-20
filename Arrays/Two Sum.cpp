4ba4b5be2d91a5a16f8a965f63f089b2608a3d6d
Sofia Ingigerth Canas Urbina <84815840+SICU2001@users.noreply.github.com>
2023-02-19 19:03:15 -0600

    Update Two Sum.cpp

--- Arrays/Two Sum.cpp
+++ Arrays/Two Sum.cpp
@@ -5,20 +5,12 @@ public:
         map<int, int> m;
         m.clear();
         for(int i=0;i<nums.size();i++){
-            if(m[nums[i]]==0){
-                m[nums[i]]=i+1;
-                if(m[target-nums[i]]!=0 && 2*nums[i]!=target){
-                mv.clear();
+            if(m[target-nums[i]]!=0){
                 mv.push_back(m[target-nums[i]]-1);
-                mv.push_back(m[nums[i]]-1);
-                return mv;
-            }
-            }
-            else if(2*nums[i]==target){
-                mv.clear();
-                mv.push_back(m[nums[i]]-1);
                 mv.push_back(i);
                 return mv;
+            }else{
+                m[nums[i]]=i+1;
             }
         }
         return mv;
